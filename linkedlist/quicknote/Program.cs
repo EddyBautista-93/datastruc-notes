357af1590042a1224ca3faa74ab6aba697f31eec
EddyBautista <bautista.j.eddy@gmail.com>
2020-11-01 08:42:03 -0600

    Add project files.

---
+++ linkedlist/quicknote/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace quicknote
+{
+    class Program
+    {
+        class LinkedListNode
+        {
+            public int data;
+            public LinkedListNode next;
+
+            // constructer for linked list
+            public LinkedListNode(int x) 
+            {
+                data = x; 
+                next = null;
+            }
+        }
+
+        class LinkedList
+        {
+            //whenever we create a link list we want a way to ref the beggining of the list.
+            LinkedListNode head;
+
+            // method to add new nodes to linked list
+            public void AddNodeToFront(int data)
+            {
+                LinkedListNode node = new LinkedListNode(data);
+                node.next = head;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+
+
+
+}
