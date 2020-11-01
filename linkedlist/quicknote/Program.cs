32b4e7e0309511b6d1fb5bab979f14353bc3291b
EddyBautista <bautista.j.eddy@gmail.com>
2020-11-01 09:04:23 -0600

    Added notesd and created const for linkedlist

--- linkedlist/quicknote/Program.cs
+++ linkedlist/quicknote/Program.cs
@@ -4,15 +4,15 @@ namespace quicknote
 {
     class Program
     {
-        class LinkedListNode
+    class LinkedListNode
         {
             public int data;
             public LinkedListNode next;
 
-            // constructer for linked list
-            public LinkedListNode(int x) 
+            // constr for linked list
+            public LinkedListNode(int x)
             {
-                data = x; 
+                data = x;
                 next = null;
             }
         }
@@ -21,21 +21,44 @@ namespace quicknote
         {
             //whenever we create a link list we want a way to ref the beggining of the list.
             LinkedListNode head;
+            int count;
 
+            public LinkedList()
+            {
+                head = null;
+                count = 0;
+            }
             // method to add new nodes to linked list
             public void AddNodeToFront(int data)
             {
                 LinkedListNode node = new LinkedListNode(data);
                 node.next = head;
+                head = node;
+                count++;
+            }
+
+            public void PrintList()
+            {
+                LinkedListNode runner = head;
+                while(runner != null)
+                {
+                    Console.WriteLine(runner.data);
+                    runner = runner.next;
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddNodeToFront(5);
+            linkedList.AddNodeToFront(7);
+            linkedList.AddNodeToFront(2);
+            linkedList.AddNodeToFront(3);
+            linkedList.AddNodeToFront(11);
+            linkedList.AddNodeToFront(72);
+            // prints backwards because the last number takes over the head
+            linkedList.PrintList();
         }
     }
-
-
-
 }
